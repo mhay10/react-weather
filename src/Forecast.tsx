88589c50d496fe507bf38f0676638e9aff79920b
mhay10 <maxwellhay1@gmail.com>
2020-12-29 00:46:00 -0700

    Got forecast url for weather api

--- src/Forecast.tsx
+++ src/Forecast.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
 interface ForecastProps {
     location: string;
-    lat: string;
-    long: string;
+    lat: number;
+    long: number;
 }
 
 export class Forecast extends React.Component<ForecastProps, {}> {
-    constructor(props: ForecastProps) {
-        super(props);
-    }
-
     render() {
         return (
             <div>
-                <table>
-                    <tr>
-                        <tr>
-                            <th>Date</th>
-                            <th>Time</th>
-                        </tr>
-                        <tr>
-
-                        </tr>
-                    </tr>
-                </table>
-                <p>Lat: {this.props.lat}, Long: {this.props.long}</p>
+                <p>Lat: {this.props.lat}, Long: {this.props.long}</p> 
             </div>
         )
     }
--- src/WeatherApp.tsx
+++ src/WeatherApp.tsx
@@ -7,19 +7,32 @@ interface WeatherAppState {
     location: string;
     lat: number;
     long: number;
+
+    forecastUrl: string;
+    forecastData: string;
 }
 
 export class WeatherApp extends React.Component<{}, WeatherAppState> {
     constructor(props: any) {
         super(props);
 
-        this.state = { location: '' , lat: 0, long: 0 };
+        this.state = {
+            location: '',
+            lat: 0,
+            long: 0,
+
+            forecastUrl: '',
+            forecastData: ''
+        };
 
         this.locationChanged = this.locationChanged.bind(this);
+        this.geocode = this.geocode.bind(this);
     }
 
     locationChanged(location: string) {
         this.setState({ location: location });
+
+        setTimeout(() => this.geocode());
     }
 
     geocode() {
@@ -30,24 +43,25 @@ export class WeatherApp extends React.Component<{}, WeatherAppState> {
 
         axios.get('https://geocode.xyz', { params }).then((res: AxiosResponse) => {
             this.setState({
-                lat: Number(res.data.latt),
-                long: Number(res.data.longt)
+                lat: res.data.latt,
+                long: res.data.longt
             });
+
+            axios.get(`https://api.weather.gov/points/${this.state.lat},${this.state.long}`).then((res: AxiosResponse) => {
+                this.setState({ forecastUrl: res.data.properties.forecast });
+            })
         });
     }
 
-    gridId() {
-        axios.get(`https://api.weather.gov/points/${this.state.lat},${this.state.long}`).then((res: AxiosResponse) => {
-            console.log(res.data);
-        })
+    forecast() {
+
     }
 
     render() {
         return (
             <div>
-                <Location onLocationChange={this.locationChanged} currentLocation={this.state.location}/>
-                <p>{this.state.location}</p>
-                <Forecast location={this.state.location} lat={this.state.lat.toString()} long={this.state.long.toString()}/>
+                <Location onLocationChange={this.locationChanged} currentLocation={this.state.location} />
+                <Forecast location={this.state.location} lat={this.state.lat} long={this.state.long} />
             </div>
         );
     }
